6b9647ef25f438037178a3955246719e0c0136e1
queercat <tusek.may@gmail.com>
2021-05-28 11:42:42 -0700

    Working on SE portion.

---
+++ software_engineering.py
@@ -0,0 +1,78 @@
+# Applying management methodologies to develop software.
+# (Git, Scrum, Agile, Unit Testing)
+
+# What is Agile...
+"""
+A software development methodology based on iterative development.
+
+Requirements and solutions evolve through collaboration between
+self organizing cross functional teams.
+
+Promotes frequent inspection and adaptation. All based off
+of the 'Agile Manifesto'
+"""
+
+# What is Scrum...
+"""
+Scrum is a lightweight process subset of agile. 
+The most widely used one.
+
+* A "process framework"
+	- Particular set of practices that must be followed.
+	- Scrum uses sprints, XP requires paired programming.
+
+* "Lightweight"
+	- Overhead is small as possible to maximise productivity.
+
+A scrum process is distinguished from others by specific
+concepts and practices.
+
+Three categories:
+* Roles
+* Artifacts
+* Time Boxes 
+
+Scrum most often used to manage complex software and
+product development.
+
+Significantly increases productivity and reduces
+time to benefits relative to waterfall processes.
+
+Scrum processes enable smooth transitions with
+rapdily-changing requirements. 
+
+Scrum benefits...
+* Increase the quality of deliverables.
+* Cope better with change.
+* Provide better estimates with less time creating them.
+* Be more in control of the project schedule and state.
+"""
+
+# Scrum Process Overview
+"""
+[Sprint Backlog -> Product Backlog] -> [Sprint]
+
+Sprint Backlog: Features assigned to sprint.
+Product Backlog: Features assigned by client.
+
+Daily scrum meeting:
+	- What has happened.
+	- What will happen.
+	- Issues.
+"""
+
+# Scrum Requirements...
+"""
+Scrum does not define specific requirements about
+how they exist, just that they go into a PB as a PBI
+(Product Backlog Item).
+
+Most scrum projects borrow from the XP practice
+describing a feature request as a 'User Story'. Although
+some use an older concept called 'Use case'.
+"""
+
+# User Story (From XP [Extreme Programming])
+"""
+Describes a required feature.
+"""
